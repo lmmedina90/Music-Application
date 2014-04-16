ba5cf4a0e5c0bfa423d8454768e4c56d62add58e
Louis <viewsatwork@gmail.com>
2014-04-15 20:02:28 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user,  only: [:index, :edit, :update, :destroy]
   before_action :correct_user,    only: [:edit, :update]
   before_action :admin_user,      only: :destroy
  	
@@ -15,6 +14,7 @@ class UsersController < ApplicationController
 
   def show
       @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def edit
